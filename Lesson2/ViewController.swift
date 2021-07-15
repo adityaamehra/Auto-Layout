6c712a2bdc69d22b193516a173062e82e1034b8b
Adityaa Mehra <adirakash@gmail.com>
2021-07-15 21:20:15 +0530

    Initial commit

---
+++ Lesson2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson2
+//
+//  Created by Adityaa Mehra on 14/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
